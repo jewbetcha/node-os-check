528cd469eb43574cd6b38925f4968a1639614832
Coleman <crollins199@gmail.com>
2018-01-10 12:58:14 -0600

    [cr] committing initial work on CLI tool

---
+++ index.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const prog = require('caporal');
+const os = require('os');
+const exec = require('child_process').exec;
+
+const runCommand = (action) => {
+    exec(action, (error, stdout, stderr) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(stderr);
+        }
+    });
+}
+
+prog
+    .version('0.1')
+    .description('A CLI tool to check your current os enviroment and run appropriate scripts based on the result.')
+    .command('check')
+    .option('--script [script]', 'Is this a script in your package.json?(y/n)', prog.REPEATABLE)
+    .option('--linux [linux]', 'Process to run if on Linux')
+    .option('--win [win]', 'Process to run if on Windows')
+    .option('--max [mac]', 'Process to run if on a Mac')
+    .action(function(args, options, logger) {
+        const platform = os.platform();
+        switch (platform) {
+            case 'linux':
+                let action = options.linux;
+                if (options.script === 'y') {
+                    action = `npm run ${action}`;
+                }
+                runCommand(action);
+                break;
+            case 'darwin':
+                let action = options.mac;
+                if (options.script === 'y') {
+                    action = `npm run ${action}`;
+                }
+                runCommand(action);
+                break;
+            case 'win32':
+                let action = options.win;
+                if (options.script === 'y') {
+                    action = `npm run ${action}`;
+                }
+                runCommand(action);
+                break;
+            default:
+                logger.info('idk wtf system this is')
+                break;
+        }
+
+    });
+
+prog.parse(process.argv);
+    

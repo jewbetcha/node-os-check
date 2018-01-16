e295f72fd9c6ebe6c1328d35aefc7976425c8303
Coleman <crollins199@gmail.com>
2018-01-16 13:07:42 -0600

    [cr] prettier format document

--- index.js
+++ index.js
@@ -1,62 +1,66 @@
 #!/usr/bin/env node
 
-const prog = require('caporal');
-const os = require('os');
-const exec = require('child_process').exec;
+const prog = require("caporal");
+const os = require("os");
+const exec = require("child_process").exec;
 
-const runCommand = (action) => {
-    exec(action, (error, stdout, stderr) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(stderr);
-        }
-    });
-}
+const runCommand = action => {
+  exec(action, (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(stderr);
+    }
+  });
+};
 
 prog
-    .version('0.1')
-    .description('A CLI tool to check your current os enviroment and run appropriate scripts based on the result.')
-    .command('check')
-    .option('--script [script]', 'Is this a script in your package.json?(y/n)', prog.REPEATABLE)
-    .option('--linux [linux]', 'Process to run if on Linux')
-    .option('--win [win]', 'Process to run if on Windows')
-    .option('--max [mac]', 'Process to run if on a Mac')
-    .action(function(args, options, logger) {
-        const platform = os.platform();
-        let action;
-	if (typeof(action) === 'undefined') {
-	    console.log(platform);	
-	} else {
-        switch (platform) {
-            case 'linux':
-                action = options.linux;
-                if (options.script === 'y') {
-                    action = `npm run ${action}`;
-                }
-                runCommand(action);
-                break;
-            case 'darwin':
-                action = options.mac;
-                if (options.script === 'y') {
-                    action = `npm run ${action}`;
-                }
-                runCommand(action);
-                break;
-            case 'win32':
-                action = options.win;
-                if (options.script === 'y') {
-                    action = `npm run ${action}`;
-                }
-                runCommand(action);
-                break;
-            default:
-                logger.info('idk wtf system this is')
-                break;
-        }
-	}
-    });
-
+  .version("0.1")
+  .description(
+    "A CLI tool to check your current os enviroment and run appropriate scripts based on the result."
+  )
+  .command("check")
+  .option(
+    "--script [script]",
+    "Is this a script in your package.json?(y/n)",
+    prog.REPEATABLE
+  )
+  .option("--linux [linux]", "Process to run if on Linux")
+  .option("--win [win]", "Process to run if on Windows")
+  .option("--max [mac]", "Process to run if on a Mac")
+  .action(function(args, options, logger) {
+    const platform = os.platform();
+    let action;
+    if (typeof action === "undefined") {
+      console.log(platform);
+    } else {
+      switch (platform) {
+        case "linux":
+          action = options.linux;
+          if (options.script === "y") {
+            action = `npm run ${action}`;
+          }
+          runCommand(action);
+          break;
+        case "darwin":
+          action = options.mac;
+          if (options.script === "y") {
+            action = `npm run ${action}`;
+          }
+          runCommand(action);
+          break;
+        case "win32":
+          action = options.win;
+          if (options.script === "y") {
+            action = `npm run ${action}`;
+          }
+          runCommand(action);
+          break;
+        default:
+          logger.info("idk wtf system this is");
+          break;
+      }
+    }
+  });
 
 prog.parse(process.argv);
-    
